83c844b6f9820b593447044ea734612a74decac9
marcbperez <marcbperez@users.noreply.github.com>
2017-02-19 16:13:27 +0100

    Wrapped main script inside IIFE and modified index page accordingly.

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
-var slideIn = function(elementId) {
-  try {
-    var element = document.getElementById(elementId);
-    element.className = element.className + " " + "slide-in";
-  } catch (e) {
-    console.log(e)
-  }
-};
+'use strict';
+
+(function () {
+
+  var setAction = function(elements, eventName, action) {
+    if (typeof elements === 'string' || elements instanceof String) {
+      elements = document.getElementsByClassName(elements);
+    }
+
+    for (var i = 0; i < elements.length; i++) {
+      elements[i]["on" + eventName] = action;
+    }
+  };
+
+  var actionSlideIn = function() {
+    var targetId = this.getAttribute("href").replace("#", "");
+    var targetElement = document.getElementById(targetId);
+
+    targetElement.className += " slide-in";
+  };
+
+  setAction("action-slide-in", "click", actionSlideIn);
+
+})();
