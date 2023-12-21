d75fde7c91b9c30c1bf3ec6bcf665ba83c3ba968
marcbperez <482530+marcbperez@users.noreply.github.com>
2023-12-21 22:41:16 +0100

    Fixed base URL and links.

--- theme/CodeHighlighter.js
+++ theme/CodeHighlighter.js
@@ -3,39 +3,70 @@ import htm from "https://esm.sh/htm";
 import { h, render } from "https://esm.sh/preact";
 import { useLayoutEffect } from "https://esm.sh/preact/hooks";
 import { useStoredState } from "./hooks/useStoredState.js";
+import { elementClass as themeFormPrefix } from "./ThemeForm.js";
 
 const html = htm.bind(h);
 
-export function CodeHighlighter() {
-  const dark = "https://esm.sh/prismjs/themes/prism-okaidia.min.css";
-  const light = "https://esm.sh/prismjs/themes/prism-solarizedlight.min.css";
+/**
+ * The ID of element this component will be rendered in.
+ * @type {string}
+ */
+const elementId = "codehighlighter";
 
-  const updateCss = () => {
-    // Resolve the theme.
+/**
+ * Light theme CSS file for Prism.
+ * @type {string}
+ */
+const lightCss = "https://esm.sh/prismjs/themes/prism-solarizedlight.min.css";
+
+/**
+ * Dark theme CSS file for Prism.
+ * @type {string}
+ */
+const darkCss = "https://esm.sh/prismjs/themes/prism-okaidia.min.css";
+
+/**
+ * Highlights multi-language code elements using Prism and keeps their style up
+ * to date with the page theme settings.
+ */
+export function CodeHighlighter() {
+  /**
+   * Creates a CSS link in the document header for Prism's theme, and updates it
+   * depending on the theme.
+   */
+  const updateCssLink = () => {
+    // Use the stored value of the theme or detect it if there is none.
     let theme = read("theme") || "detect";
     if (theme === "detect") {
       theme = matchMedia("(prefers-color-scheme: dark)").matches
         ? "dark" : "light";
     }
-    // Set Prism's CSS element.
-    let cssLink = document.getElementById("code-highlighter-theme");
+    // Create the link element if it does not exist.
+    let cssLink = document.getElementById(`${elementId}-theme`);
     if (!cssLink) {
       cssLink = document.createElement("link");
       cssLink.rel = "stylesheet";
-      cssLink.id = "code-highlighter-theme";
+      cssLink.id = `${elementId}-theme`;
       document.head.appendChild(cssLink);
     }
-    // Set Prism's theme.
-    cssLink.href = theme === "dark" ? dark : light;
+    // Set the CSS of the link element according to the theme.
+    cssLink.href = theme === "dark" ? darkCss : lightCss;
   }
 
-  const { read } = useStoredState("themeform", updateCss);
+  /**
+   * Update the CSS link every time the theme from the "stored state" changes.
+   */
+  const { read } = useStoredState(themeFormPrefix, updateCssLink);
 
+  /**
+   * Highlights `code` elements of multiple languages using Prism every time the
+   * page loads.
+   */
   useLayoutEffect(() => {
-    updateCss();
-
-    // Set the highlighter for each code element.
+    // Set the theme for Prism first, then initialise the elements.
+    updateCssLink();
     [...document.querySelectorAll("code")].forEach(code => {
+      // Get the highlighter for this element or fall back to `language-markup`.
       const highlighter = (
         code.parentElement.nodeName === "PRE" &&
         code.parentElement.getAttribute("class")
@@ -43,17 +74,20 @@ export function CodeHighlighter() {
         ? `language-${code.parentElement.getAttribute("class")}`
         : "language-markup";
       code.classList.add(highlighter);
+      // Set the messages shown when clicking on the "copy to clipboard" button.
       code.setAttribute("data-prismjs-copy", "📋");
       code.setAttribute("data-prismjs-copy-success", "✅");
       code.setAttribute("data-prismjs-copy-error", "❌");
     });
-    // Highlight the code elements.
+    // Highlight all the code elements in the page.
     try {
       Prism.highlightAll();
     } catch (error) { }
   }, []);
 
+  // This component modifies external elements only.
   return null;
 }
 
-render(html`<${CodeHighlighter} />`, document.getElementById("code-highlighter"));
+// Render this component in an element with ID like `elementId`.
+render(html`<${CodeHighlighter} />`, document.getElementById(elementId));
--- theme/ThemeForm.js
+++ theme/ThemeForm.js
@@ -2,45 +2,125 @@ import htm from "https://esm.sh/htm";
 import { h, render } from "https://esm.sh/preact";
 import { useState, useLayoutEffect } from "https://esm.sh/preact/hooks";
 import { useStoredState } from "./hooks/useStoredState.js";
-import { Checkbox, Select } from "./components/FormComponents.js";
+import { Select, Checkbox, CheckboxValue } from "./components/FormComponents.js";
 
 const html = htm.bind(h);
 
+/**
+ * The CSS class of the elements this component will be rendered in, and the
+ * prefix for its "stored-state".
+ * @type {string}
+ */
+export const elementClass = "themeform";
+
+/**
+ * Types of fields allowed in the form rendered by this component.
+ * @readonly
+ * @enum {string}
+ */
+const FieldType = {
+  SELECT: "select",
+  CHECKBOX: "checkbox",
+};
+
+/**
+ * Form field definition and behaviours.
+ * @typedef {Object} Field
+ * @property {FieldType} type Type of the field.
+ * @property {string[]} [options] Optional. Options for type `FieldType.SELECT`.
+ * @property {string} default Default field value.
+ * @property {function} onChange Executed every time the field changes.
+ * @property {function} updateBody Executed after the field changes.
+ */
+
+/**
+ * Renders a form with components that change the CSS classes of the document
+ * body, letting the user customise the theme of the page.
+ */
 export function ThemeForm() {
-  // Adds the selected option as css class to the body if it exists,
-  // and removes the css classes of all the other options from the body.
+  /**
+   * Adds the selected option as CSS class to the body and removes the rest,
+   * given the ID of a select field and its value, the selected option.
+   * @param {string} id The ID of the select field.
+   * @param {string} value The value of the select field.
+   */
   const updateBodyFromSelect = (id, value) => {
     for (let i = 0; i < fields[id]["options"].length; i++) {
-      if (fields[id]["options"][i] === value) {
-        document.body.classList.add(fields[id]["options"][i]);
+      const option = fields[id]["options"][i];
+      if (option === value) {
+        // Add the selected option as CSS class to the body.
+        document.body.classList.add(option);
       } else {
-        document.body.classList.remove(fields[id]["options"][i]);
+        // Remove the CSS class of the option that is not selected.
+        document.body.classList.remove(option);
       }
     }
   };
 
-  // Adds the checkbox id as css class to the body if checked,
-  // otherwise removes the css class from the body.
+  /**
+   * Adds a checkbox ID as CSS class to the body when checked, or removes it
+   * otherwise, given the ID of a checkbox field and its value.
+   * @param {string} id The ID of the checkbox field.
+   * @param {CheckboxValue} value The value of the checkbox field.
+   */
   const updateBodyFromCheckbox = (id, value) => {
-    (value === "on")
+    (value === CheckboxValue.ON)
       ? document.body.classList.add(id)
       : document.body.classList.remove(id);
   };
 
-  // Form values, shared through "stored state", because there might be several
-  // instances of this component in the same page (ie. header and footer).
+  /**
+   * Returns the first option to appear as a CSS class in the body, or nothing
+   * if none of them are present.
+   * @param {string[]} options The options of the select element.
+   * @returns {string|undefined} The option found, or nothing.
+   */
+  const selectValueFromBody = (options) => {
+    for (let i = 0; i < options.length; i++) {
+      const option = options[i];
+      if (document.body.classList.contains(option)) {
+        return option;
+      }
+    }
+  };
+
+  /**
+   * Returns the positive value for a checkbox if its ID appears as a CSS class
+   * in the body, or the negative value otherwise.
+   * @param {string} id The ID of the checkbox.
+   * @returns {CheckboxValue} The found value of the checkbox.
+   */
+  const checkboxValueFromBody = (id) => {
+    return document.body.classList.contains(id)
+      ? CheckboxValue.ON
+      : CheckboxValue.OFF;
+  }
+
+  /**
+   * Form values shared through "stored state", because there might be several
+   * instances of this component in the same page (ie. header and footer).
+   */
   const [values, setValues] = useState({});
-  // Load the "stored state" into the form values after calling "save", without
-  // updating the body classes.
-  const { save, read } = useStoredState("themeform", () => {
+
+  /**
+   * Load the "stored state" into the form values after every call to `save`
+   * without updating the CSS classes in the body.
+   */
+  const { save, read } = useStoredState(elementClass, () => {
     let values = {};
     for (const [key, field] of Object.entries(fields)) {
+      // Use the stored value or the field's default if there is none.
       values[key] = read(key) || field["default"];
     }
-
+    // Update the form.
     setValues(values);
   });
 
+  /**
+   * Saves the value of a select element in "stored state" and updates the CSS
+   * classes of the body according to the element, when the element changes.
+   * @param {Event} e The change event of the select element.
+   */
   const onChangeSelect = (e) => {
     const { id, value } = e.currentTarget;
     const { updateBody } = fields[id];
@@ -48,19 +128,57 @@ export function ThemeForm() {
     updateBody(id, value);
   };
 
+  /**
+   * Saves the value of a checkbox element in "stored state" and updates the CSS
+   * classes of the body according to the element, when the element changes.
+   * @param {Event} e The change event of the checkbox element.
+   */
   const onChangeCheckbox = (e) => {
     const { id, checked } = e.currentTarget;
     const { updateBody } = fields[id];
-    const value = checked ? "on" : "off";
+    const value = checked ? CheckboxValue.ON : CheckboxValue.OFF;
     save(id, value);
     updateBody(id, value);
   };
 
+  /**
+   * Load the "stored state" into the form values and update the CSS classes in
+   * the body according to each field every time the page loads.
+   */
+  useLayoutEffect(() => {
+    let values = {};
+    for (const [key, field] of Object.entries(fields)) {
+      // Use the stored value or the field's default if there is none.
+      values[key] = read(key) || field["default"];
+      field.updateBody(key, values[key]);
+    }
+    // Update the form.
+    setValues(values);
+  }, []);
+
+  // Options and default value for the theme selector.
+  const themeOptions = ["detect", "light", "dark"];
+  const themeDefault = "detect";
+
+  // Options and default value for the font size selector.
+  const textOptions = ["small", "medium", "large"];
+  const textDefault = "medium";
+
+  // Options and default value for the font family selector.
+  const fontOptions = ["monospace", "sans-serif", "serif"];
+  const fontDefault = "serif";
+
+  /**
+   * Form configuration object with keys representing element ID's, and values
+   * containing field paremeters.
+   * @type {Object.<string, Field>}
+   */
   const fields = {
+    /** Theme name. */
     theme: {
-      type: "select",
-      options: ["detect", "light", "dark"],
-      default: "detect",
+      type: FieldType.SELECT,
+      options: themeOptions,
+      default: selectValueFromBody(themeOptions) || themeDefault,
       onChange: onChangeSelect,
       updateBody: (id, value) => {
         let detected;
@@ -68,53 +186,48 @@ export function ThemeForm() {
           detected = matchMedia("(prefers-color-scheme: dark)").matches
             ? "dark" : "light";
         }
-
+        // Use the detected theme instead of "detect" if that was the value, or
+        // use the selected value if not.
         updateBodyFromSelect(id, detected || value);
       },
     },
+    /** Font size. */
     text: {
-      type: "select",
-      options: ["small", "medium", "large"],
-      default: "medium",
+      type: FieldType.SELECT,
+      options: textOptions,
+      default: selectValueFromBody(textOptions) || textDefault,
       onChange: onChangeSelect,
       updateBody: updateBodyFromSelect,
     },
+    /** Font family. */
     font: {
-      type: "select",
-      options: ["monospace", "sans-serif", "serif"],
-      default: "monospace",
+      type: FieldType.SELECT,
+      options: fontOptions,
+      default: selectValueFromBody(fontOptions) || fontDefault,
       onChange: onChangeSelect,
       updateBody: updateBodyFromSelect,
     },
+    /** Layout width. */
     stretch: {
-      type: "checkbox",
-      default: "off",
+      type: FieldType.CHECKBOX,
+      default: checkboxValueFromBody("stretch") || CheckboxValue.OFF,
       onChange: onChangeCheckbox,
       updateBody: updateBodyFromCheckbox,
     },
   };
 
-  useLayoutEffect(() => {
-    let values = {};
-    for (const [key, field] of Object.entries(fields)) {
-      values[key] = read(key) || field["default"];
-      field.updateBody(key, values[key]);
-    }
-
-    setValues(values);
-  }, []);
-
+  // Show a form with the fields from above.
   return html`<form>
     ${Object.entries(fields).map(([key, field]) => {
     switch (field["type"]) {
-      case "select":
+      case FieldType.SELECT:
         return html`<${Select}
           id=${key}
           value=${values[key]}
           onChange=${field["onChange"]}
           options=${field["options"]}
         />`;
-      case "checkbox":
+      case FieldType.CHECKBOX:
         return html`<${Checkbox}
           id=${key}
           value=${values[key]}
@@ -127,7 +240,8 @@ export function ThemeForm() {
   </form>`;
 }
 
-const elements = document.getElementsByClassName("theme-form");
+// Render this component in elements with a CSS class like `elementClass`.
+const elements = document.getElementsByClassName(elementClass);
 for (let i = 0; i < elements.length; i++) {
   render(html`<${ThemeForm} />`, elements[i]);
 }
